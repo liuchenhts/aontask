7b135e743f42c701dadcdeeb67ee61fd91590c33
Chen Liu <liuchenhts@gmail.com>
2018-03-07 21:28:50 +1100

    add docs

--- proenvrun.sh
+++ proenvrun.sh
@@ -2,4 +2,4 @@ docker-compose -f docker-compose.pro.yml up -d
 
 # or run: docker run --rm -p 8080:8080 liuchen/aontask
 # kubectl create -f k8s_deployment.yml
-# kubectl create -f k8s_service.yml 
+# kubectl create -f k8s_service.yml
